4dda750b77a5935f9ff323d85090bf88daa2f1b9
Sbuhatem <rcphdsamir@gmail.com>
2022-09-13 16:46:01 -0300

    Migrate

--- app1/models/Estoque_Individual.py
+++ app1/models/Estoque_Individual.py
@@ -3,8 +3,8 @@ from app1.models import *
 
 class Estoque_Individual(models.Model):
     estoqueCreate = models.DateTimeField(verbose_name="TimeStamp",auto_now_add=True,blank=True)
-    estoque_pessoa_nome = models.ManyToManyField(Pessoas, on_delete=models.CASCADE, verbose_name='Residente')
-    estoque_usos_consumo = models.ManyToManyField(Uso_Consumo, on_delete=models.CASCADE, verbose_name='Produto')
+    estoque_pessoa_nome = models.ManyToManyField(Pessoas, verbose_name='Residente')
+    estoque_usos_consumo = models.ManyToManyField(Uso_Consumo, verbose_name='Produto')
     estoque_movimento = models.CharField(max_length=1, choices=Entrada_Saida, default='')
     estoque_quantidade = models.IntegerField(verbose_name='Quantidade', default=0)
     estoque_saldo = models.IntegerField(verbose_name='Saldo em Estoque', default=0)
@@ -15,6 +15,5 @@ class Estoque_Individual(models.Model):
         return self.estoque_pessoa_nome.pessoa_nome
 
     class Meta:
-        ordering = ['estoque_pessoa_nome']
         verbose_name = 'Estoque Individual'
         verbose_name_plural = 'Estoque Individuais'
