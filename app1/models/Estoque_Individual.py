73231d1f83a642d584759a1cf29794851bd4facf
Sbuhatem <rcphdsamir@gmail.com>
2022-09-15 11:26:53 -0300

    novo DROP

--- app1/models/Estoque_Individual.py
+++ app1/models/Estoque_Individual.py
@@ -4,7 +4,7 @@ from app1.models import *
 
 class Estoque_Individual(models.Model):
     estoqueCreate = models.DateTimeField(verbose_name="TimeStamp",auto_now_add=True,blank=True)
-    estoque_pessoa_nome = models.CharField(max_length=50)
+    estoque_pessoa_nome = models.ForeignKey(Pessoas, on_delete=models.CASCADE, verbose_name='Residente')
     estoque_usos_consumo = models.ManyToManyField(Uso_Consumo, verbose_name='Produto')
     estoque_movimento = models.CharField(max_length=1, choices=Entrada_Saida, default='')
     estoque_quantidade = models.IntegerField(verbose_name='Quantidade', default=0)
@@ -13,7 +13,11 @@ class Estoque_Individual(models.Model):
 
 
     def __str__(self):
-        return self.estoque_usos_consumo
+        itens = ''
+        for i in self.estoque_usos_consumo.all():
+            itens += i.consumo_nome
+
+        return f'{self.estoque_pessoa_nome.pessoa_nome}, {itens}'
 
     class Meta:
         verbose_name = 'Estoque Individual'
