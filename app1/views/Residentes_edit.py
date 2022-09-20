7c0b99b17eeaf92d12acbef9e1b4c9cc466d6c8e
Sbuhatem <rcphdsamir@gmail.com>
2022-09-20 08:25:06 -0300

    Commit atualizacao

--- app1/views/Residentes_edit.py
+++ app1/views/Residentes_edit.py
@@ -4,19 +4,19 @@ from app1.views import *
 @login_required
 def Residentes_edit(request, id):
     residente = get_object_or_404(Pessoas, pk=id)
-    form = Uso_ConsumoForm(instance=residente)
+    form = ResidentesForm(instance=residente)
     if (request.method == 'POST'):
         form = ResidentesForm(request.POST, instance=residente)
         if (form.is_valid()):
             residente.save()
             return redirect('/residentes_list')
         else:
-            return render(request, 'residentes_edit.html', {
+            return render(request, '/residentes_edit', {
                 'form': form, 
                  'residente': residente
              })
     else:
-        return render(request, 'residentes_edit.html', {
+        return render(request, '/residentes_edit', {
             'form': form,
             'residente': residente
         })
