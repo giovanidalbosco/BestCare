14fa8d9164d659a3c9934bf480e7429ac6d20297
Jackson Castellain <jgcastellain@gmail.com>
2022-09-25 11:00:59 -0300

    base_inicial

--- proj/settings.py
+++ proj/settings.py
@@ -145,7 +145,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGIN_REDIRECT_URL = '/telainicial'
+LOGIN_REDIRECT_URL = '/base_telainicial'
 LOGOUT_REDIRECT_URL = '/accounts/login'
 
 #Email
