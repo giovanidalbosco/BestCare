dede40d88f13932a647503ca0983857387163173
Sbuhatem <rcphdsamir@gmail.com>
2022-09-27 11:46:48 -0300

    Alteracoes Models

--- app1/models/Pessoas.py
+++ app1/models/Pessoas.py
@@ -9,7 +9,7 @@ class Pessoas(models.Model):
     pessoa_numero = models.CharField(max_length=10, verbose_name='Numero', blank=True, null=True)
     pessoa_compl = models.CharField(max_length=30, verbose_name='Complemento', blank=True, null=True)
     pessoa_cidade = models.ForeignKey(Cidades, on_delete=models.CASCADE, verbose_name='Cidade', default='')
-    pessoa_CPF = models.CharField(max_length=11, verbose_name='CPF', default=0, unique=True)
+    pessoa_CPF = models.CharField(max_length=11, verbose_name='CPF', default='', unique=True)
     pessoa_classe = models.IntegerField(choices=CLASSE, blank=False, null=False)
     pessoa_telefone  = models.CharField(max_length=20, verbose_name='Telefone', blank=True, null=True, unique=True)
     pessoa_email = models.EmailField(verbose_name='E-mail', blank=True, null=True, unique=True)
--- app1/views/Ocorrencias_list.py
+++ app1/views/Ocorrencias_list.py
@@ -5,8 +5,8 @@ from app1.views import *
 def Ocorrencias_list(request):
     search = request.GET.get('search')
     if search:
-        ocorrencia = Ocorrencias.objects.filter(pessoa_classe__icontains=('2','1'))
+        ocorrencia = Ocorrencias.objects.filter(ocorrencia_pessoa_nome__icontains='2')
     else:
-        ocorrencia = Ocorrencias.objects.all()
+        ocorrencia = Ocorrencias.objects.filter(ocorrencia_pessoa_nome__icontains='2')
 
     return render(request, 'ocorrencias_list.html', {'ocorrencia': ocorrencia})
