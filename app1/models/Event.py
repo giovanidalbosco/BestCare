b09176456ae5da9d171d0ba75a357cae98b69207
Lucas Rezende <lucasrezende97@yahoo.com.br>
2022-09-27 09:07:33 -0300

    juntando agenda e prescrição

--- app1/models/Event.py
+++ app1/models/Event.py
@@ -3,6 +3,10 @@ from app1.models import *
 
 class Event(models.Model):
     title = models.CharField(max_length=200)
+    prescricao_pessoa_nome = models.ForeignKey(Pessoas, on_delete=models.CASCADE, verbose_name="Residente", default='')
+    prescricao_consumo_nome = models.ForeignKey(Uso_Consumo, on_delete=models.CASCADE, verbose_name="Produto", default='')
+    prescricao_frequencia = models.IntegerField(choices=FREQUENCIA, default='')
+    prescricao_dose = models.IntegerField(choices=DOSE, default='')
     description = models.TextField()
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
