d4ad9e267588b8488f38a568b37e15f49a68432f
Jackson Castellain <jgcastellain@gmail.com>
2022-10-06 10:15:41 -0300

    pop-up

--- app1/static/JS/index.js
+++ app1/static/JS/index.js
@@ -6,3 +6,10 @@ function alerta(){
 //    confirm("Deseja excluir permanentemente o item?"); 
 //
 //};
+
+const modal = document.getElementById('modal');
+
+function toggleModal() {
+    modal.classList.toggle('modal-visible');
+
+  }
