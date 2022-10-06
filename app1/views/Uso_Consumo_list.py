0afc126029d9ee14662438e06d3f03bbfca37d87
Lucas Rezende <lucasrezende97@yahoo.com.br>
2022-10-06 10:36:39 -0300

    alterações front-end html

--- app1/views/Uso_Consumo_list.py
+++ app1/views/Uso_Consumo_list.py
@@ -5,9 +5,8 @@ from app1.views import *
 def Uso_Consumo_list(request):
     search = request.GET.get('search')
     if search:
-        uso_consumo = Uso_Consumo.objects.filter(consumo_nome__icontains=search)
+        uso_consumo = Uso_Consumo.objects.filter(uso_consumo_consumo_nome__icontains=search)
     else:
         uso_consumo = Uso_Consumo.objects.all()
-
 
     return render(request, 'uso_consumo_list.html', {'uso_consumo': uso_consumo})
