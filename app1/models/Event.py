f923247e6fb3a27d4e8ec4db4e84b2e6a92bf2ee
Sbuhatem <rcphdsamir@gmail.com>
2022-10-10 15:04:44 -0300

    Verbouse_name Event

--- app1/models/Event.py
+++ app1/models/Event.py
@@ -1,14 +1,15 @@
+from tabnanny import verbose
 from app1.models import *
 
 
 class Event(models.Model):
-    title = models.IntegerField(choices=EVENTO, default='')
+    title = models.IntegerField(choices=EVENTO, default='', verbose_name= 'Evento')
     event_pessoa_nome = models.ForeignKey(Pessoas, on_delete=models.CASCADE, verbose_name="Residente", default='')
-    event_consumo_nome = models.ForeignKey(Medicamentos, on_delete=models.CASCADE, verbose_name="Uso_consumo", default='')
-    event_dose = models.IntegerField(choices=DOSE, default=0)
-    description = models.TextField()
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
+    event_consumo_nome = models.ForeignKey(Medicamentos, on_delete=models.CASCADE, verbose_name='Medicamento', default='')
+    event_dose = models.IntegerField(choices=DOSE, default=0, verbose_name='Dose')
+    description = models.TextField(verbose_name='Descricao')
+    start_time = models.DateTimeField(verbose_name='Dia/Hora - Inicio')
+    end_time = models.DateTimeField(verbose_name='Dia/Hora - Fim')
 
     @property
     def get_html_url(self):
