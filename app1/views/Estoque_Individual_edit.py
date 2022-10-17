88f19016ed3b9de8b9d86922ed2300014e42f4ff
GIOVANI VALLE DALBOSCO <giovani.dalbosco@univille.br>
2022-10-17 15:34:52 -0300

    resolvido save do edit many to many para estoque individual

--- app1/views/Estoque_Individual_edit.py
+++ app1/views/Estoque_Individual_edit.py
@@ -8,7 +8,10 @@ def Estoque_Individual_edit(request, id):
     if (request.method == 'POST'):
         form = Estoque_IndividualForm(request.POST, instance=estoque_individual)
         if (form.is_valid()):
+            estoque_individual = form.save(commit=False)
             estoque_individual.save()
+            if hasattr(form, 'save_m2m'):
+                form.save_m2m()
             return redirect(f'/estoque_individual_list/?search={estoque_individual.estoque_pessoa_nome.pessoa_nome}')
         else:
             return render(request, 'estoque_individual_form.html', {
